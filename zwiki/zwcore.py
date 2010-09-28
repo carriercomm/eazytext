ec4a00437cbc7ba9e8012638c0c3d09d938631f7
Pratap Chakravarthy <pratap@K2-AMDX2>
2010-09-28 09:22:11 +0530

    Documentation changes.

--- zwiki/zwcore.py
+++ zwiki/zwcore.py
@@ -1,7 +1,3 @@
-# This file is subject to the terms and conditions defined in
-# file 'LICENSE', which is part of this source code package.
-#       Copyright (c) 2010 SKR Farms (P) LTD.
-
 # -*- coding: utf-8 -*-
 
 """Everything can be a component.
